779f3c21c14ec2c666d44ac5c12549b86937bfe3
Jeremy Artiflas <Artiflas@gmail.com>
2021-04-11 17:21:48 +0200

    Possibility to mkdir an fileoutput

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 var flagOutput = flag.String("o", "", "file as target")
@@ -13,6 +14,9 @@ var flagOutput = flag.String("o", "", "file as target")
 func main() {
 	flag.Parse()
 	args := flag.Args()
+	var w io.Writer
+	// default is Stdout
+	w = os.Stdout
 	if len(args) != 1 {
 		fmt.Println("please just one URL")
 		os.Exit(1)
@@ -24,5 +28,20 @@ func main() {
 		os.Exit(2)
 	}
 	defer resp.Body.Close()
-	io.Copy(os.Stdout, resp.Body)
+	if *flagOutput != "" {
+		path := filepath.Dir(*flagOutput)
+		err := os.MkdirAll(path, 0755)
+		if err != nil {
+			fmt.Println("cannot create folder", path, err)
+			os.Exit(22)
+		}
+		fd, err := os.OpenFile(*flagOutput, os.O_CREATE|os.O_WRONLY, 0755)
+		if err != nil {
+			fmt.Println("error creating file", err, *flagOutput)
+			os.Exit(20)
+		}
+		defer fd.Close()
+		w = fd
+	}
+	io.Copy(w, resp.Body)
 }
